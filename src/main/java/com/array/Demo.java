e5cc63aac580f1be6cede0bae7f656048a7de2ae
ridhi <ridhigujral2702@gmail.com>
2025-05-22 13:02:33 +0530

    Added com.array package

---
+++ src/main/java/com/array/Demo.java
@@ -0,0 +1,14 @@
+package com.array;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;
+int marks[]=new int[5];
+System.out.println(marks);
+System.out.println(x);
+	}
+
+}
+ 
